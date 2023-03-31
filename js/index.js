f6957063568c4c4ac83a363ead1a49271ccf3cbe
Julio Alvarez <JulioJAM019@gmail.com>
2023-03-30 22:25:11 -0400

    Fix cors

--- js/index.js
+++ js/index.js
@@ -1,15 +1,21 @@
-const username = document.getElementById("username").value;
-const password = document.getElementById("password").value;
 
-fetch("https://localhost:3000/api/access",{
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({username: username, password: password})
-})
-.then(response => {
-    console.log(response);
-})
-.catch(error => {
-    console.log(error);
-    alert("Error al iniciar sesión");
+
+document.getElementById("login-btn").addEventListener("click", function(){
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    fetch("http://localhost:3000/api/user",{
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({username: username, password: password})
+    })
+    /*.then(response => {
+        console.log(response);
+    })
+    .catch(error => {
+        console.log(error);
+        alert("Error al iniciar sesión");
+    });*/
+    .then(response => response.json())
+    .catch(error => console.error(error));
 });
