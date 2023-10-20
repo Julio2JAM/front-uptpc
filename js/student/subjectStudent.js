5b4bda45c1fdbe00f0dc6d986985adb0041966b1
Julio <julio18@biyuyo.org>
2023-10-20 15:39:43 -0400

    In dev

--- js/student/subjectStudent.js
+++ js/student/subjectStudent.js
@@ -45,3 +45,74 @@ async function classroom(){
     });
     
 }
+
+async function loadData(idEnrollment){
+    await fetch(`${API_URL}/enrollment/program/?program=${idEnrollment}`)
+        .then(response => response.json())
+        .then(data => dataTable(data))
+        .catch(err => err)
+}
+
+function dataTable(data){
+    const tbody = document.querySelector("tbody");
+    tbody.innerHTML = "";
+
+    const statusData = {
+        "-1": "Eliminado",
+        "0": "No disponible",
+        "1": "Disponible"
+    };
+    
+    const statusClass = {
+      "-1": "deleted",
+      "0": "unavailable",
+      "1": "available"
+    };
+
+    // Crear boton de view
+    const button = document.createElement('button');
+    button.innerHTML = "Ver más";
+    button.className = "view-button";
+    data.forEach(element => {
+        const row = tbody.insertRow(-1);
+
+        const id = row.insertCell(0);
+        id.innerHTML = element.id;
+
+        const profressor = row.insertCell(1);
+        profressor.innerHTML = element.profressor.person.name + ' ' + element.profressor.person.lastname;
+
+        const subject = row.insertCell(2);
+        subject.innerHTML = element.subject.name;
+
+        const status = row.insertCell(4);
+        const statusSpan = document.createElement('span');
+        statusSpan.innerHTML = statusData[element.id_status];
+        statusSpan.classList.add("status", statusClass[element.id_status]);
+        status.appendChild(statusSpan);
+
+        const action = row.insertCell(5);
+        action.appendChild(button.cloneNode(true));
+    });
+
+    addEvents();
+}
+
+// Agregar evento de click a todos los botones de view
+function addEvents(){
+    const buttons = document.querySelectorAll("tbody button");
+    buttons.forEach(button => button.addEventListener("click", async(event) => await detail(event)));
+}
+
+// Obtener todos los datos de un elemento
+async function detail(event){
+
+    const row = event.target.closest('tr');
+    const id = row.cells[0].textContent;
+
+    await fetch(`${API_URL}/subject/?id=${id}`)
+    .then(response => response.json())
+    .then(data => createModalBox(data[0]))
+    .catch(err => console.error(err));
+
+}
