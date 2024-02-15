5cb9f379da6d7f188b5121a7d8263dc54085e7a1
Julio <julio18@biyuyo.org>
2024-02-15 15:06:55 -0400

    Fix professor enrollment module

--- js/professor/enrollment.js
+++ js/professor/enrollment.js
@@ -1,59 +1,35 @@
-API_URL = 'http://localhost:3000/api'
+const API_URL = "http://localhost:3000/api"
+const token = sessionStorage.getItem('token');
+var printData = [];
 
-document.getElementById("select-classroon").addEventListener("click", async () => await classroom());
-async function classroom(){
 
-    // Array de cookies, separadas por ';'.
-    const cookies = document.cookie.split(';');
+// Agregar evento de click para mostrar una lista con todas las secciones activas.
+document.getElementById("select-classroon").addEventListener("click", async () => await createModalList());
 
-    // Nombre de la cookie a buscar.
-    const cookieName = "token";
+// Crear modal box con el nombre de las secciones activas.
+async function createModalList(){
 
-    // Si el array esta vacio, quiere decir que no hay cookies registradas.
-    if(!cookies){
-        return;
-    }
-
-    // Buscar en las cookies
-    let cookieValue;
-    for (const value of cookies) {
-        // Validar que la cookie evaluada, comience por el nombre que se está buscando.
-        if (value.startsWith(cookieName)) {
-            // Obtener el valor de la cookie, eliminando el nombre +1 caracter, porque al obtener el array de cookies, los valores obtenidos son tipo JSON, es decir "nombre:valor", de esta manera, se elimina "nombre:", dejando solo el valor.
-            cookieValue = value.substring(cookieName.length + 1);
-            // Decodificar el valor obtenido y asignarlo a la variable anteriormente declarada 'cookieValue'.
-            cookieValue = decodeURIComponent(cookieValue);
-            // Detener el ciclo de busqueda.
-            break;
-        }
-    }
-
-    // Validar que se haya obtenido una cookie, es decir, que cookieValue no este vacia.
-    if (!cookieValue) {
-        return;
-    }
-    
+    // const validateModalMenu = document.getElementById("modal-menu");
     const div = document.createElement("div");
     div.id = "modal-menu";
     div.className = "modal-menu";
 
     const ul = document.createElement("ul");
+    ul.id = "classList";
 
-    await fetch(`${API_URL}/enrollment/`, {
+    await fetch(`${API_URL}/enrollment/`,{
         method: "GET",
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
-        },
-        credentials: 'include'
+        headers: {authorization: 'Bearer ' + token}
     })
     .then(response => response.json())
     .then(data => {
+        console.log(data);
         data.forEach(element => {
             const li = document.createElement("li");
             const a = document.createElement("a");
-            a.innerHTML = element?.classroom.name;
-            a.addEventListener("click", async () => await loadData(element.id))
+            a.innerHTML = element.classroom.name;
+            a.addEventListener("click", () => loadClassroomEvents(element.classroom.id, element.classroom.name))
+
             li.appendChild(a);
             ul.appendChild(li);
         });
@@ -70,62 +46,75 @@ async function classroom(){
     
     div.appendChild(ul);
     document.body.appendChild(div);
-
+    
     const modalMenu = document.getElementById("modal-menu");
     modalMenu.addEventListener("click", event => {
         if (event.target.id === "modal-menu" || event.target.nodeName == "A") {
             document.getElementById("modal-menu").remove();
         }
     });
-    
 }
 
-async function loadData(idEnrollment){
-    await fetch(`${API_URL}/enrollment/program/?enrollment=${idEnrollment}`)
+// Agregar eventos de click para a la lista de secciones, para obtener datos de la seccion seleccionada.
+async function loadClassroomEvents(id, name){
+    const classroom = document.getElementById("classroom");
+    // classroom.value = name;
+    classroom.value = id;
+
+    await fetch(`${API_URL}/enrollment/?idClassroom=${id}`,{
+        method: 'GET',
+        headers: {authorization: 'Bearer ' + token}
+    })
     .then(response => response.json())
     .then(data => dataTable(data))
-    .catch(err => err)
+    .catch(error => error);
+
 }
 
-function dataTable(data){
+// Cargar registros en la tabla HTML
+function dataTable(data) {
+
     const tbody = document.querySelector("tbody");
     tbody.innerHTML = "";
+
+    const button = document.createElement("button");
+    button.className = "view-button";
+    button.innerText = "Ver mas";
 
     const statusData = {
         "-1": "Eliminado",
         "0": "No disponible",
         "1": "Disponible"
     };
-    
+
     const statusClass = {
-      "-1": "deleted",
-      "0": "unavailable",
-      "1": "available"
+        "-1": "deleted",
+        "0": "unavailable",
+        "1": "available"
     };
 
-    // Crear boton de view
-    const button = document.createElement('button');
-    button.innerHTML = "Ver más";
-    button.className = "view-button";
     data.forEach(element => {
         const row = tbody.insertRow(-1);
 
         const id = row.insertCell(0);
-        id.innerHTML = element.id;
+        id.innerText = element.id;
 
-        const profressor = row.insertCell(1);
-        profressor.innerHTML = element.professor.person.name + ' ' + element.professor.person.lastName;
+        const name = row.insertCell(1);
+        name.innerText = element?.student.person.name ?? "No name";
 
-        const subject = row.insertCell(2);
-        subject.innerHTML = element.subject.name;
+        const lastname = row.insertCell(2);
+        lastname.innerText = element?.student.person.lastName ?? "No last name";
 
-        const status = row.insertCell(3);
+        const cedule = row.insertCell(3);
+        cedule.innerText = element?.student.person.cedule;
+
+        const status = row.insertCell(4);
         const statusSpan = document.createElement('span');
-        statusSpan.innerHTML = statusData[element.id_status];
+        statusSpan.innerText = statusData[element.id_status];
         statusSpan.classList.add("status", statusClass[element.id_status]);
         status.appendChild(statusSpan);
-
-        const action = row.insertCell(4);
+        
+        const action = row.insertCell(5);
         action.appendChild(button.cloneNode(true));
     });
 
@@ -146,143 +135,7 @@ async function detail(event){
 
     await fetch(`${API_URL}/program/?id=${id}`)
     .then(response => response.json())
-    .then(data => createModalBox(data[0]))
+    .then(data => data/*createModalBox(data[0])*/)
     .catch(err => console.error(err));
 
-}
-
-function createModalBox(data){
-
-    // Crear divs contenedores
-    var modal = document.createElement("div");
-    modal.className = "modal";
-    modal.id = "modal";
-
-    var modalContent = document.createElement("div");
-    modalContent.className = "modal-content";
-
-    var header = document.createElement("header");
-    
-    // Crear elementos del DOM
-    var h3 = document.createElement("h3");
-    var img = document.createElement("img");
-    img.src = "../../source/professor2-icon.png";
-    var buttonClose = document.createElement("button");
-    buttonClose.className = "close-btn";
-    buttonClose.innerHTML = "&times;"
-
-    var section = document.createElement("section");
-    var form = document.createElement("form");
-    form.id = "modal-form";
-
-    var labelId = document.createElement("label");
-    labelId.for = "id";
-    labelId.innerHTML = "ID:";
-    labelId.style.display = "none";
-    var inputId = document.createElement("input");
-    inputId.type = "text";
-    inputId.id = "id";
-    inputId.placeholder = "ID";
-    inputId.value = data?.id ?? "";
-    inputId.style.display = "none";
-
-    var labelProfessor = document.createElement("label");
-    labelProfessor.for = "professor";
-    labelProfessor.innerHTML = "Docente:";
-    var inputProfessor = document.createElement("input");
-    inputProfessor.type = "text";
-    inputProfessor.id = "professor";
-    inputProfessor.placeholder = "Docente";
-    inputProfessor.value = data.professor.person?.name + " " + data.professor.person?.lastName;
-    
-    var labelSubject = document.createElement("label");
-    labelSubject.for = "subject";
-    labelSubject.innerHTML = "Asignatura:";
-    var inputSubject = document.createElement("input");
-    inputSubject.type = "text";
-    inputSubject.id = "subject";
-    inputSubject.placeholder = "Asignatura";
-    inputSubject.value = data.subject.name;
-    
-    var labelStatus = document.createElement("label");
-    var selectStatus = document.createElement("select");
-    var options = [
-        {value: -1, label: "Eliminado"},
-        {value: 0, label: "No disponible"},
-        {value: 1, label: "Disponible"}
-    ];
-    labelStatus.textContent = "Estado";
-    selectStatus.id = "status";
-    for (var option of options) {
-        selectStatus.add(new Option(option.label, option.value));
-    }
-    selectStatus.value = data?.id_status ?? 1;
-    selectStatus.disabled = true;
-
-    var footer = document.createElement("footer");
-
-    /*
-    var buttonSubmit = document.createElement("button");
-    buttonSubmit.addEventListener("click", async () => {});
-    buttonSubmit.type = "submit";
-    buttonSubmit.id = "save";
-    buttonSubmit.innerHTML = data?.id ? "Actualizar" : "Crear";
-
-    var buttonReset = document.createElement("button");
-    buttonReset.type = "reset";
-    buttonReset.id = "reset";
-    buttonReset.innerHTML = "Borrar";
-    buttonReset.addEventListener("click", () => {
-        inputId.value = data?.id ?? "";
-        inputName.value = data?.name ?? "";
-        inputDescription.value = data?.description ?? "";
-        selectStatus.value = data?.id_status ?? 1;
-    });
-    */
-
-    h3.appendChild(img);
-    h3.innerHTML += "Asignatura";
-
-    header.appendChild(h3);
-    header.appendChild(buttonClose);
-    
-    form.appendChild(labelId);
-    form.appendChild(inputId);
-
-    form.appendChild(labelProfessor);
-    form.appendChild(inputProfessor);
-
-    form.appendChild(labelSubject);
-    form.appendChild(inputSubject);
-
-    form.appendChild(labelStatus);
-    form.appendChild(selectStatus);
-
-    section.appendChild(form);
-    
-    // footer.appendChild(buttonSubmit);
-    // footer.appendChild(buttonReset);
-    
-    modalContent.appendChild(header);
-    modalContent.appendChild(section);
-    modalContent.appendChild(footer);
-
-    modal.appendChild(modalContent);
-    document.body.appendChild(modal);
-
-    buttonClose.addEventListener("click", closeModal);
-    modal.addEventListener("click", (event) => {
-        if(event.target.id == "modal"){
-            closeModal();
-        }
-    });
-
-    function closeModal() {
-        modal.classList.add("close-modal");
-        setTimeout(() => {
-            modal.style.display = "none";
-            modal.classList.remove("close-modal");
-            modal.remove();
-        }, 260);
-    }
 }
